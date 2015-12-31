c3087c86f4dbdcaed8fc80482f385bf45826d203
Willie <williereed@hotmail.com>
2015-12-30 17:46:40 -0800

    forgot this file

---
+++ src/MyBase/Node.java
@@ -0,0 +1,17 @@
+package MyBase;
+
+/**
+ * Created by W on 12/29/2015.
+ */
+public class Node {
+    int v;
+    Node l;
+    Node r;
+
+    public Node(int val) {
+        v = val;
+        l = null;
+        r = null;
+
+    }
+}
